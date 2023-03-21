aaf654965e1e9370360a6b5356823c03778ff672
Mwakanemela Kayange - Coming from the commandline <kayangemwakanemela@gmail.com>
2023-03-21 22:56:20 +0300

    Updated server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from bot server
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://m-ai.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
